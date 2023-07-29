e4f6992b1f13f864ea08ef3ddde4ed60ffa7e546
yamax1 <128352531+yamax1@users.noreply.github.com>
2023-07-29 19:34:56 +0900

    Update bmi calculate.py

--- bmi calculate.py
+++ bmi calculate.py
@@ -1,18 +1,18 @@
 import PySimpleGUI as sg
-sg.theme("DarkBrown3")
+sg.theme("White")
 
 layout = [[sg.T("身長と体重を入力")],
           [sg.T("身長cm"),sg.I("160", k="in1")],
           [sg.T("体重kg"),sg.I("60", k="in2")],
           [sg.B(" 実行 ", k="btn"),sg.T(k="txt")]]
-win = sg.Window("BMI値計算アプリ", layout,
+win = sg.Window("BMI値計算", layout,
                 font=(None,14), size=(320,150))
 
 def execute():
     in1 = float(v["in1"])/100.0
     in2 = float(v["in2"])
     bmi = in2 / (in1 * in1)
-    txt = f"BMI値は、{bmi:.2f}です。"
+    txt = f"あなたのBMI値は、{bmi:.2f}です。"
     win["txt"].update(txt)
 
 while True:
@@ -21,4 +21,5 @@ while True:
       execute()
     if e == None:
         break
+              
 win.close()
